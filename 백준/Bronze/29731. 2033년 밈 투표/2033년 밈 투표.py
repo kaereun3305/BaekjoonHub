eacc9ebbdcf820f45c4c73d4f2bd363c413f70e8
kaereun3305 <115064255+kaereun3305@users.noreply.github.com>
2024-11-29 15:42:23 +0900

    [Bronze V] Title: 2033년 밈 투표, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/29731. 2033년 밈 투표/2033년 밈 투표.py
@@ -0,0 +1,18 @@
+rick = ['Never gonna give you up',
+'Never gonna let you down',
+'Never gonna run around and desert you',
+'Never gonna make you cry',
+'Never gonna say goodbye',
+'Never gonna tell a lie and hurt you',
+'Never gonna stop']
+
+T = int(input())
+check = False
+for _ in range(T):
+    n = input()
+    if n not in rick:
+        check = True
+        break
+if check:
+    print("Yes")
+else: print("No")
